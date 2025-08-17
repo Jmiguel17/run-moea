6e6eba70552bfa0fbcf0f22030c8e794e70e2d8b
Jose Gonzalez <jose@Joses-MacBook-Pro.local>
2025-08-17 18:44:07 +0100

    adding new platypus wrapper

--- run_moea/BsonPlatypusWrapper.py
+++ run_moea/BsonPlatypusWrapper.py
@@ -3,6 +3,7 @@ import uuid
 import gzip
 import random
 import datetime
+import platypus
 
 import numpy as np
 import pandas as pd
@@ -148,3 +149,70 @@ class LoggingArchive(Archive):
     def add(self, solution):
         super().add(solution)
         self.log.append(np.array([o for o in solution.objectives]))
+        
+
+class SaveNondominatedSolutionsArchive(PlatypusWrapper):
+
+    def __init__(self, *args, **kwargs):
+        self.output_directory = kwargs.pop('output_directory', 'outputs')
+        self.search_data = kwargs.pop('search_data', {})
+        self.model_name = kwargs.pop('model_name')
+        super().__init__(*args, **kwargs)
+
+        # To determine the number of variables, etc
+        m = self.model
+
+    @property
+    def output_subdirectory(self):
+        path = os.path.join(self.output_directory, self.model_name[0:-4], f'{self.uid}_seed{self.search_data["seed"]}')
+        os.makedirs(path, exist_ok=True)
+        return path
+
+    def save_nondominant(self, algorithm):
+
+        uid = uuid.uuid4().hex
+
+        va = []
+        va_ = []
+        nva_ = []
+
+        platypus_variables = platypus.nondominated(algorithm.result)
+        for v in platypus_variables:
+            for ivar, var in enumerate(self.model_variables):
+                j = slice(self.model_variable_map[ivar], self.model_variable_map[ivar+1])
+                tem_va = np.array(v.variables[j])
+                tem_nme = [f'{var.name}[d{i}]' for i in range(len(tem_va))]
+
+                va_.append(tem_va)
+                nva_.append(tem_nme)
+
+            va_flatt = [val for sublist in va_ for val in sublist]
+            nva_flatt = [val for sublist in nva_ for val in sublist]
+            va.append(va_flatt)
+            va_ = []
+            nva_ = []
+
+        variables = pd.DataFrame(data=va, columns=nva_flatt)
+        variables.columns = variables.columns.str.replace(r"[_]", "")
+        variables = variables.add_prefix('MT_MIN_')
+
+        platypus_objectives = platypus.nondominated(algorithm.result)
+
+        objectives = pd.DataFrame(data=np.array([o.objectives for o in platypus_objectives]),
+                                  columns=[f'{o.name}' for o in self.model_objectives])
+
+        objectives.columns = objectives.columns.str.replace(r"[_]", "")
+        objectives = objectives.add_prefix('MO_MAX_')
+
+        platypus_constraints = platypus.nondominated(algorithm.result)
+
+#        TODO allow to pass double bounded constraints
+        constraints = pd.DataFrame(data=np.array([c.constraints for c in platypus_constraints]),
+                                   columns=[f'{c.name}' for c in self.model_constraints])
+
+        constraints.columns = constraints.columns.str.replace(r"[_]", "")
+        constraints = constraints.add_prefix('MT_MIN_')
+
+        metrics = pd.concat([objectives, constraints, variables], axis=1)
+        metrics['NFE'] = algorithm.nfe
+        metrics.to_csv(os.path.join(self.output_subdirectory, f'i{uid}_fe{algorithm.nfe}.csv.gz'), compression='infer')
